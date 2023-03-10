ef26e4c983bbdd0baea01919b59ab28c3a435e56
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-10 06:18:16 +0530

    falte3button change to textbutton

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flat_3d_button/flat_3d_button.dart';
 
 class NewTransaction extends StatelessWidget {
   final Function addTx;
@@ -39,10 +38,12 @@ class NewTransaction extends StatelessWidget {
                 keyboardType: TextInputType.number,
                 onSubmitted: (_) => submitData(),
               ),
-              Flat3dButton(
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(fontSize: 16),
+                ),
                 onPressed: submitData,
                 child: Text('Add Transaction'),
-                color: Colors.brown,
               ),
             ]),
       ),
